fe57f763b1e5ffa54b133ed84ef4de50b84a4c8b
Mersad17 <uramersad17@gmail.com>
2025-12-19 15:44:25 +0100

    added icon

--- app.config.ts
+++ app.config.ts
@@ -4,12 +4,18 @@ const config: ExpoConfig = {
   name: "birthdayly-mobile",
   slug: "birthdayly-mobile",
   scheme: "birthdayly",
+  icon: "./assets/icon.png",
   android: {
     package: 'com.mersadura.birthdayly',
+    adaptiveIcon: {
+      foregroundImage: "./assets/icon.png",
+      backgroundColor: "#FFFFFF",
+    },
   },
 
   ios: {
     bundleIdentifier: 'com.mersadura.birthdayly',
+    icon: "./assets/icon.png",
   },
   extra: {
     API_BASE_URL: process.env.EXPO_PUBLIC_API_BASE_URL,
@@ -17,6 +23,12 @@ const config: ExpoConfig = {
       projectId: '96199366-28f3-491d-9e30-f08b3f1cd9b8',
     },
   },
+  splash: {
+    image: "./assets/splash.png",
+    resizeMode: "contain",
+    backgroundColor: "#ffffff",
+  },
+  
 };
 
 export default config;
